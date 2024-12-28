92cec7c92b5e989bd4d5ac81b457fc06864eb313
Solara <fbp3sgxt@students.codeinstitute.net>
2024-12-28 21:49:08 +0000

    add overview view

--- todo/views.py
+++ todo/views.py
@@ -6,3 +6,8 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import generic
 from .models import List, Item
+
+
+class Overview(generic.ListView):
+    queryset = List.objects.all()
+    template_name = "todo/home.html"
