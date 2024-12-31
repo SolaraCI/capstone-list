2b9cb82662daf1d84a800a31c7b4650ad9c64a2d
Solara <fbp3sgxt@students.codeinstitute.net>
2024-12-31 05:33:34 +0000

    remove summernote url

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -20,5 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('todo.urls'), name='home'),
     path('admin/', admin.site.urls),
-    path('summernote/', include('django_summernote.urls')),
 ]
