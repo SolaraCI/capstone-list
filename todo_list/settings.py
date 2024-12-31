90b3bcf20de429c22933f0bebff90921acb16d70
Solara <fbp3sgxt@students.codeinstitute.net>
2024-12-31 05:30:56 +0000

    add custom css

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_summernote",
     "cloudinary_storage",
     "cloudinary",
     "todo",
@@ -136,7 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'todo/static'), ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
