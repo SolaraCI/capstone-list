9028c073eb280577be65dfc5864b59ab460f504c
Solara <fbp3sgxt@students.codeinstitute.net>
2025-01-03 22:47:37 +0000

    add view_list view

--- todo/urls.py
+++ todo/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.Overview.as_view(), name='home'),
+    path('lists/<int:list_id>/', views.SingleListView.as_view(), name='list_view'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -11,3 +11,16 @@ from .models import List, Item
 class Overview(generic.ListView):
     queryset = List.objects.all()
     template_name = "todo/index.html"
+    
+    
+class SingleListView(generic.ListView):
+    template_name = '/workspace/capstone-list/todo/templates/todo/view_list.html'
+    
+    def get_queryset(self):
+        self.parent_list = get_object_or_404(List, id=self.kwargs.get('list_id'))
+        return Item.objects.filter(parent_list=self.parent_list)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['parent_list'] = self.parent_list
+        return context
