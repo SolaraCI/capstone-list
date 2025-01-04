32ad341d1b61e870090eae0fc0defdc2f575695b
Solara <fbp3sgxt@students.codeinstitute.net>
2025-01-04 03:49:56 +0000

    update listform

--- todo/forms.py
+++ todo/forms.py
@@ -3,16 +3,16 @@ from .models import List, Item
 from django.db import models
 from django.contrib.auth.models import User
 
-# class ListForm(forms.ModelForm):
-#     class Meta:
-#         model = List
-#         fields = ['name', 'description', 'creator', 'ordered']
-#         initial = {
-#             'creator': models.ForeignKey(User, on_delete=models.CASCADE)
-#         }
-#         widgets = {
-#             'creator': forms.HiddenInput()
-#         }
+class ListForm(forms.ModelForm):
+    class Meta:
+        model = List
+        fields = ['title', 'description', 'creator']
+        initial = {
+            'creator': models.ForeignKey(User, on_delete=models.CASCADE)
+        }
+        widgets = {
+            'creator': forms.HiddenInput()
+        }
 
 
 # class ItemForm(forms.ModelForm):
