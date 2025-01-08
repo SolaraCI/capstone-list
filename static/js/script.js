3d3e5dca5584cbe4f94435d5949fed04b0d59d87
Solara <fbp3sgxt@students.codeinstitute.net>
2025-01-08 19:18:23 +0000

    enable submission of form content

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,32 @@
-let createItemButton = document.getElementById("create-item-button");
-let itemContainer = document.getElementById("item-container");
-let newItemInput = document.getElementById("new-item-input");
+document.addEventListener("DOMContentLoaded", function () {
+  var createItemButton = document.getElementById("create-item-button");
+  var itemContainer = document.getElementById("item-container");
+  var formActive = false;
 
+  // Add event listener to the create item button
+  createItemButton.addEventListener("click", function () {
+    // If the form is not active, create a form
+    if (formActive == false) {
+      let inputForm = document.createElement("input");
+      inputForm.type = "text";
+      inputForm.placeholder = "Enter new item name";
+      inputForm.formMethod = "post";
+      inputForm.formAction = "submit";
+      inputForm.classList.add("form-control", "mt-3");
+      itemContainer.appendChild(inputForm);
+      formActive = true;
+    }
+    // If the form is active, remove the form and create a new item
+    else {
+      let inputForm = document.querySelector("input");
+      let itemName = inputForm.value;
+      let item = document.createElement("div");
+      item.classList.add("item");
+      item.innerHTML = itemName;
+      itemContainer.appendChild(item);
+      itemContainer.removeChild(inputForm);
+      formActive = false;
 
-createItemButton.addEventListener("click", function () {
-    let inputForm = document.createElement("input");
-    inputForm.type = "text";
-    inputForm.placeholder = "Enter new item name";
-    inputForm.classList.add("form-control", "mt-3");
-    itemContainer.appendChild(inputForm);
+    }
   });
+});
