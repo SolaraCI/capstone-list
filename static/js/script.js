1919c7072242f0528de6b19bb6b8d7777f3a2cea
Solara <fbp3sgxt@students.codeinstitute.net>
2025-01-08 19:50:01 +0000

    create submitItem function

--- static/js/script.js
+++ static/js/script.js
@@ -3,6 +3,18 @@ document.addEventListener("DOMContentLoaded", function () {
   var itemContainer = document.getElementById("item-container");
   var formActive = false;
 
+  // Function to submit the item
+  function submitItem() {
+    let inputForm = document.querySelector("input");
+    let itemName = inputForm.value;
+    let item = document.createElement("div");
+    item.classList.add("item");
+    item.innerHTML = itemName;
+    itemContainer.appendChild(item);
+    itemContainer.removeChild(inputForm);
+    formActive = false;
+  }
+
   // Add event listener to the create item button
   createItemButton.addEventListener("click", function () {
     // If the form is not active, create a form
@@ -15,18 +27,16 @@ document.addEventListener("DOMContentLoaded", function () {
       inputForm.classList.add("form-control", "mt-3");
       itemContainer.appendChild(inputForm);
       formActive = true;
+      // Add event listener to the input form
+      inputForm.addEventListener("keypress", function (e) {
+        if (e.key === "Enter" && inputForm.value != "") {
+          submitItem();
+        }
+      });
     }
     // If the form is active, remove the form and create a new item
-    else {
-      let inputForm = document.querySelector("input");
-      let itemName = inputForm.value;
-      let item = document.createElement("div");
-      item.classList.add("item");
-      item.innerHTML = itemName;
-      itemContainer.appendChild(item);
-      itemContainer.removeChild(inputForm);
-      formActive = false;
-
+    else if (formActive == true && inputForm.value != "") {
+      submitItem();
     }
   });
 });
