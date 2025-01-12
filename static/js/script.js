f2078d44d947b6e1113d2b7693ec2ab5c88b8012
Solara <fbp3sgxt@students.codeinstitute.net>
2025-01-12 05:37:59 +0000

    correct variable name item-form -> itemForm, remove obsolete code

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var itemForm = document.getElementById("item-form");
+  var itemForm = document.getElementById("itemForm");
   var itemContainer = document.getElementById("item-container");
 
+  // Add event listener to the item form to allow creation of new items
   itemForm.addEventListener("submit", function (e) {
     e.preventDefault();
 
@@ -35,3 +36,45 @@ document.addEventListener("DOMContentLoaded", function () {
       });
   });
 });
+
+// var createItemButton = document.getElementById("create-item-button");
+// var itemContainer = document.getElementById("item-container");
+// var formActive = false;
+
+// // Function to submit the item
+// function submitItem() {
+//   let inputForm = document.querySelector("input");
+//   let itemName = inputForm.value;
+//   let item = document.createElement("div");
+//   item.classList.add("item");
+//   item.innerHTML = itemName;
+//   itemContainer.appendChild(item);
+//   itemContainer.removeChild(inputForm);
+//   formActive = false;
+// }
+
+// // Add event listener to the create item button
+// createItemButton.addEventListener("click", function () {
+//   // If the form is not active, create a form
+//   if (formActive == false) {
+//     let inputForm = document.createElement("input");
+//     inputForm.type = "text";
+//     inputForm.placeholder = "Enter new item name";
+//     inputForm.formMethod = "post";
+//     inputForm.formAction = "submit";
+//     inputForm.classList.add("form-control", "mt-3");
+//     itemContainer.appendChild(inputForm);
+//     formActive = true;
+//     // Add event listener to the input form
+//     inputForm.addEventListener("keypress", function (e) {
+//       if (e.key === "Enter" && inputForm.value != "") {
+//         submitItem();
+//       }
+//     });
+//   }
+//   // If the form is active, remove the form and create a new item
+//   else if (formActive == true && inputForm.value != "") {
+//     submitItem();
+//   }
+// });
+// });
