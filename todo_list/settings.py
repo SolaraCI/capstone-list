08a9884a7c75a8b4acdc1bf709a56d169dc914c9
Solara <fbp3sgxt@students.codeinstitute.net>
2025-01-15 02:07:48 +0000

    prepare for deployment

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -31,7 +31,6 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
 ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
@@ -104,13 +103,6 @@ WSGI_APPLICATION = "todo_list.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
 
