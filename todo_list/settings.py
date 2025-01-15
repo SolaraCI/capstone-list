9faefa91b4633d0760220a8dc9359799a5b36432
Solara <fbp3sgxt@students.codeinstitute.net>
2025-01-15 02:41:21 +0000

    prepare for deployment

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -147,6 +147,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
